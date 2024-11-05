189cf4b233593d9d924250fc1ef851d7da58934e
Lorie Crettex <lorie.crettex@hes-so.ch>
2024-11-05 13:49:06 +0100

    modificatation des variables DB dans index.php 2

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+
+// The base path under which the application is exposed. For example, if you are
+// accessing the application at
+// "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
+// should be "/comem-archidep-php-todo-exercise/". If you are accessing the
+// application at "http://localhost:8888", then BASE_URL should be "/".
 define('BASE_URL', getenv('TODOLIST_BASE_URL') ?: '/');
 
 // Database connection parameters.
@@ -8,20 +14,6 @@ define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', getenv('TODOLIST_DB_NAME'));
 define('DB_HOST', getenv('TODOLIST_DB_HOST'));
 define('DB_PORT', getenv('TODOLIST_DB_PORT'));
-
-// The base path under which the application is exposed. For example, if you are
-// accessing the application at
-// "http://localhost:8888/comem-archidep-php-todo-exercise/", then BASE_URL
-// should be "/comem-archidep-php-todo-exercise/". If you are accessing the
-// application at "http://localhost:8888", then BASE_URL should be "/".
-define('BASE_URL', '/comem-archidep-php-todo-exercise/');
-
-// Database connection parameters.
-define('DB_USER', 'todolist');
-define('DB_PASS', 'Yverdon*1234');
-define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
 
 $db = new PDO('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 $items = array();
